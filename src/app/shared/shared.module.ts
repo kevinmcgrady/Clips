edca1f06bd4013cc4846a9b756890888231fc561
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-09-03 19:53:41 +0100

    created tabs component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModelComponent } from './model/model.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModelComponent],
+  declarations: [ModelComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [ModelComponent],
+  exports: [ModelComponent, TabsContainerComponent, TabComponent],
 })
 export class SharedModule {}
