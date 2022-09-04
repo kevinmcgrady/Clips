6dbd3e51c65ef58632f1978c05e895a331cad731
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-09-04 09:11:21 +0100

    Added inout field and validation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ModelComponent } from './model/model.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [ModelComponent, TabsContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModelComponent, TabsContainerComponent, TabComponent],
+  declarations: [
+    ModelComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModelComponent,
+    TabsContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModelComponent } from './auth-model/auth-model.component';
 import { SharedModule } from '../shared/shared.module';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AuthModelComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [AuthModelComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
   exports: [AuthModelComponent],
 })
 export class UserModule {}
