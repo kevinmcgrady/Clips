2ed47b2953b5b89ee912021b7d3db806b2fb0b1b
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-09-05 11:48:35 +0100

    added async validator to check for email taken

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,13 +3,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import IUser from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
 
   showAlert = false;
   inSubmission = false;
@@ -17,7 +18,11 @@ export class RegisterComponent {
   alertColor = 'blue';
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
