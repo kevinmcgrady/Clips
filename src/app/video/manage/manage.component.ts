77dedd431e3cf485fa8f638646f9527e1e5fa995
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-10-01 17:05:16 +0100

    added sort function to manage page

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
   styleUrls: ['./manage.component.css'],
 })
 export class ManageComponent implements OnInit {
-  constructor() {}
+  videoOrder = '1';
 
-  ngOnInit(): void {}
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params) => {
+      this.videoOrder = params.sort === '2' ? params.sort : '1';
+    });
+  }
+
+  sort(event: Event) {
+    const { value } = event.target as HTMLSelectElement;
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {
+        sort: value,
+      },
+    });
+  }
 }
