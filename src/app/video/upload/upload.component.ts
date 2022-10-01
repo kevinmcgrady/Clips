e45b38c4a9c3a3a6d67bd2a056b96da10b10828c
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-10-01 22:07:06 +0100

    add file upload button

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -40,7 +40,10 @@ export class UploadComponent {
 
   storeFile($event: Event) {
     this.isDraggedOver = false;
-    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    this.file = ($event as DragEvent).dataTransfer
+      ? ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+      : ($event.target as HTMLInputElement).files?.item(0) ?? null;
 
     if (!this.file || this.file.type !== 'video/mp4') {
       return;
