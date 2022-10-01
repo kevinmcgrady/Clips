8feef856e9a72044a294c65f96a3e015c05362d0
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-10-01 21:57:53 +0100

    save file details in database

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs/operators';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import { ClipService } from '../../services/clip.service';
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
@@ -18,6 +21,7 @@ export class UploadComponent {
   inSubmission: boolean = false;
   percentage: number = 0;
   showPercentage: boolean = false;
+  user: firebase.User | null = null;
 
   title = new FormControl('', {
     validators: [Validators.required, Validators.minLength(3)],
@@ -26,7 +30,13 @@ export class UploadComponent {
 
   uploadForm = new FormGroup({ title: this.title });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth,
+    private clipsService: ClipService
+  ) {
+    this.auth.user.subscribe((user) => (this.user = user));
+  }
 
   storeFile($event: Event) {
     this.isDraggedOver = false;
@@ -41,6 +51,7 @@ export class UploadComponent {
   }
 
   uploadFile() {
+    this.uploadForm.disable();
     this.showAlert = true;
     this.showPercentage = true;
     this.alertColor = 'blue';
@@ -51,6 +62,7 @@ export class UploadComponent {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipReference = this.storage.ref(clipPath);
 
     task.percentageChanges().subscribe((progress) => {
       this.percentage = (progress as number) / 100;
@@ -58,15 +70,27 @@ export class UploadComponent {
 
     task
       .snapshotChanges()
-      .pipe(last())
+      .pipe(
+        last(),
+        switchMap(() => clipReference.getDownloadURL())
+      )
       .subscribe({
-        next: () => {
+        next: (url) => {
+          const clip = {
+            uid: this.user?.uid as string,
+            displayName: this.user?.displayName as string,
+            title: this.title.value,
+            fileName: `${clipFileName}.mp4`,
+            url,
+          };
+          this.clipsService.createClip(clip);
           this.alertColor = 'green';
           this.alertMessage =
             'Success your clip is now ready to share with the world.';
           this.showPercentage = false;
         },
         error: () => {
+          this.uploadForm.enable();
           this.alertColor = 'red';
           this.alertMessage = 'Upload failed! Please try again later';
           this.inSubmission = true;
