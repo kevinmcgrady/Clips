5c3a7f2a1b5dc10a0717a2ed408435bad6f48aa0
Kev Mcgrady <kev@Kevs-MacBook-Pro.local>
2022-10-05 10:56:04 +0100

    add prod firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyALiNF_pInVsYT6avP_XUDz6B_8rWmwhOM',
+    authDomain: 'clipz-cac62.firebaseapp.com',
+    projectId: 'clipz-cac62',
+    storageBucket: 'clipz-cac62.appspot.com',
+    messagingSenderId: '172326594028',
+    appId: '1:172326594028:web:aea4a29cfc02b164c14cc9',
+  },
 };
